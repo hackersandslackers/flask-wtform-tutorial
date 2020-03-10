2ad589440b948655671ba129379e5dfbdf910c64
Krogsager <andkr@aarhus.dk>
2020-03-10 09:26:27 +0100

    Update __init__.py
    Issue: the application crashes when you try to navigate to a form from the main page index.html.
    Error message: KeyError: 'A secret key is required to use CSRF.'
    Solution: Set secret key.

--- flask_wtf_tutorial/__init__.py
+++ flask_wtf_tutorial/__init__.py
@@ -1,11 +1,12 @@
 """Initialize app."""
 from flask import Flask
-
+import os
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
+    app.secret_key = os.urandom(24) # Do not use a random key in production. Only for testing.
     app.config['RECAPTCHA_PUBLIC_KEY'] = 'iubhiukfgjbkhfvgkdfm'
     app.config['RECAPTCHA_PARAMETERS'] = {'size': '100%'}
 
